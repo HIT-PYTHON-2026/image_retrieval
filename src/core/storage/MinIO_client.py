4199fe7c52eb7b2a78871d614693ee5e0240dbb2
tung51652-alt <tung51652@gmail.com>
2026-02-14 22:23:14 +0700

    fix: rewrite path for MinIO's images

--- src/core/storage/MinIO_client.py
+++ src/core/storage/MinIO_client.py
@@ -37,14 +37,6 @@ class MinioClient:
             _, ext = os.path.splitext(image_path)
             if not ext: ext = ".jpg" # Phòng hờ không có đuôi
             final_object = f"{object_id}{ext}"
-            if(bucket_name == BUCKET_NAME):
-                a, ext = os.path.split(image_path)
-
-                final_object = f"{object_id}.{ext}"
-            else:
-                _, ext = os.path.splitext(image_path)
-                if not ext: ext = ".jpg" # Phòng hờ không có đuôi
-                final_object = f"{object_id}{ext}"
                 
             # đẩy ảnh lên minio
             self.minio.fput_object(bucket_name= bucket_name,
